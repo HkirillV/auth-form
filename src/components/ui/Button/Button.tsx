574c15003587c481cce7edfe10e8a06b35dad31c
HkirillV <khlebov79@gmail.com>
2025-05-19 21:42:23 +0300

    create button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,11 +1,42 @@
-import React from "react";
+import { PropsWithChildren } from "react";
+import classNames from "classnames";
 
-const Button = () => {
+type IButton = PropsWithChildren<{
+  type?: HTMLButtonElement["type"];
+  href?: string;
+  className?: string;
+  target?: HTMLAnchorElement["target"];
+  mode?: string;
+  onClick?: () => void;
+}>;
+
+const Button = (props: IButton) => {
+  const {
+    type = "button",
+    onClick,
+    href,
+    children,
+    className = "",
+    target,
+    mode,
+  } = props;
+
+  const isLink = href !== undefined;
+  const Component = isLink ? "a" : "button";
+  const linkProps = { href, target };
+  const buttonProps = { type };
+  const specificProps = isLink ? linkProps : buttonProps;
 
   return (
-    <div className="button">
-
-    </div>
+    <Component
+      className={classNames(className, {
+        [`button--${mode}`]: mode,
+      })}
+      {...specificProps}
+      onClick={onClick}
+    >
+      {children}
+    </Component>
   );
 };
 
